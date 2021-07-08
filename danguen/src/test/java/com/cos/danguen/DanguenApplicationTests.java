2d1a90928103b36dd45eba5c2dc74a4e0a7c96c8
Jung-jinwoo <wlsdn960319@gmail.com>
2021-07-08 10:57:26 +0900

    2021-07-08
    패키지 / app/pro / views

---
+++ danguen/src/test/java/com/cos/danguen/DanguenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.danguen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanguenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
